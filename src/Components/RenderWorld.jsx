ff5335629868f78cd0848315c257ab7b22b56e8d
PabloPicas0 <pawel.fe@gmail.com>
2023-04-09 23:22:57 +0200

    Created draggable earth

--- src/Components/RenderWorld.jsx
+++ src/Components/RenderWorld.jsx
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
 import { feature } from "topojson-client";
 
-const url = "https://unpkg.com/world-atlas@2.0.2/countries-50m.json";
+const url = "https://unpkg.com/world-atlas@2.0.2/countries-110m.json";
 
 // Dimmensions for svg element
 const w = 960;
@@ -11,53 +11,71 @@ const h = 600;
 const margin = { top: 30, right: 10, bottom: 30, left: 15 };
 
 const RenderWorld = () => {
-  const [country, setCountry] = useState(null);
   const svgRef = useRef(null);
 
   // Useful source documentation about crating world map
   // Source: https://observablehq.com/@d3/world-map-svg
   const outline = { type: "Sphere" };
   const graticule = d3.geoGraticule10();
-  let projection = d3.geoOrthographic().scale(250).center([0, 0]).rotate([0, -20]);
+  let projection = d3.geoOrthographic().scale(230).center([0, 0]).rotate([0, -20]);
   let path = d3.geoPath(projection);
 
-  const svg = d3.select(svgRef.current);
-
-  const drag = d3.drag().on("drag", (e) => {
-    console.log(e)
-  });
-
-  svg.call(drag);
-
   useEffect(() => {
+    // Selected svg elemnt using d3 and react ref
+    const svg = d3.select(svgRef.current);
+
+    // Drag method on svg element
+    const drag = d3.drag().on("drag", (e) => {
+      // projection.rotate() give us array with x, y, z coordinates
+      const [x, y] = projection.rotate();
+      const sens = 80 / projection.scale();
+
+      // We are changing projection coordinates taking old coords
+      // And adding ammount of x and y from cursor on drag event
+      projection.rotate([x + e.dx * sens, y - e.dy * sens]);
+
+      svg.selectAll("path").attr("d", path);
+    });
+
+    svg.call(drag);
+
+    // Creating conatiner
+    const container = svg
+      .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`)
+      .attr("id", "container");
+
+    const globe = container.append("g").attr("class", "globe");
+
+    // Creating new g element that will have all coutries path
+    const countries = container.append("g").attr("class", "countries");
+
+    // Adding two path elements thaht give outline and graticule
+    globe.append("path").datum(outline).attr("class", "sphere").attr("d", path);
+    globe.append("path").datum(graticule).attr("class", "graticule").attr("d", path);
+
     d3.json(url).then((response) => {
       const geojsonCountries = feature(response, response.objects.countries);
 
-      setCountry(geojsonCountries);
+      // After fetching data we create path elements in countries g elemnt
+      countries
+        .selectAll("path")
+        .data(geojsonCountries.features)
+        .enter()
+        .append("path")
+        .attr("class", "country")
+        .attr("d", path);
     });
+
+    // Cleanup on unmounting component
+    return () => {
+      svg.select("#container").remove();
+    };
   }, []);
 
   return (
     <>
-      {!country ? (
-        <h1>Loading...</h1>
-      ) : (
-        <svg ref={svgRef} width={w} height={h}>
-          <g transform={`translate(${margin.left}, ${margin.top})`}>
-            <g id="country-map">
-              <path className="sphere" d={path(outline)}></path>
-              <path d={path(graticule)} className="graticule"></path>
-              {country.features.map((feature) => (
-                <path
-                  key={feature.properties.name}
-                  id={feature.id}
-                  className="country"
-                  d={path(feature)}></path>
-              ))}
-            </g>
-          </g>
-        </svg>
-      )}
+      <svg ref={svgRef} width={w} height={h}></svg>
     </>
   );
 };
