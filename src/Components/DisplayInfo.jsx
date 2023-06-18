6835863bc1950aa608dea5380c2736954e97b09d
PabloPicas0 <pawel.fe@gmail.com>
2023-06-18 03:10:29 +0200

    DisplayInfo component is more flexible with sreen width below 400px

--- src/Components/DisplayInfo.jsx
+++ src/Components/DisplayInfo.jsx
@@ -1,6 +1,18 @@
-import { Card, CardContent, Paper, Typography } from "@mui/material";
+import { Card, CardContent, Paper, ThemeProvider, Typography, createTheme } from "@mui/material";
 
 import { useParams } from "../Context/context";
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 400,
+      md: 900,
+      lg: 1100,
+      xl: 1536,
+    },
+  },
+});
 
 const DisplayInfo = () => {
   const { basicInfo } = useParams();
@@ -26,28 +38,31 @@ const DisplayInfo = () => {
   };
 
   return (
-    <Paper
-      className="info-displayer"
-      style={{ transform: "translateX(500px)", transition: "transform 250ms ease-in-out" }}>
-      <aside>
-        <h2 style={{ textAlign: "center" }}>About Country</h2>
-        <Card sx={{ maxWidth: "400px" }}>
-          <CardContent>
-            <Typography variant="h6" component={"p"}>
-              Country: {name}
-            </Typography>
-            <Typography variant="h6">Capital: {capital}</Typography>
-            <Typography variant="h6">
-              Languages: {handleLanguages(languages).map((language) => `${language[1]}, `)}
-            </Typography>
-            <Typography variant="h6">Population: {handleNumberFormat(population)}</Typography>
-            <Typography variant="h6">Area: {handleNumberFormat(area)}</Typography>
-          </CardContent>
-        </Card>
+    <ThemeProvider theme={theme}>
+      <Paper
+        className="info-displayer"
+        style={{ transform: "translateX(500px)", transition: "transform 250ms ease-in-out" }}
+        sx={{ width: { xs: window.innerWidth, sm: "400px" }, zIndex: 1337 }}>
+        <aside>
+          <h2 style={{ textAlign: "center" }}>About Country</h2>
+          <Card>
+            <CardContent>
+              <Typography variant="h6" component={"p"}>
+                Country: {name}
+              </Typography>
+              <Typography variant="h6">Capital: {capital}</Typography>
+              <Typography variant="h6">
+                Languages: {handleLanguages(languages).map((language) => `${language[1]}, `)}
+              </Typography>
+              <Typography variant="h6">Population: {handleNumberFormat(population)}</Typography>
+              <Typography variant="h6">Area: {handleNumberFormat(area)}</Typography>
+            </CardContent>
+          </Card>
 
-        <div>test 2</div>
-      </aside>
-    </Paper>
+          <div>test 2</div>
+        </aside>
+      </Paper>
+    </ThemeProvider>
   );
 };
 
