e4e67b596b4e1308f6052eabd82a015ac5a15831
PabloPicas0 <pawel.fe@gmail.com>
2023-06-18 03:33:01 +0200

    added button in DisplayInfo component for hiding

--- src/Components/DisplayInfo.jsx
+++ src/Components/DisplayInfo.jsx
@@ -1,6 +1,8 @@
-import { Card, CardContent, Paper, ThemeProvider, Typography, createTheme } from "@mui/material";
+import { Card, CardContent, IconButton, Paper, ThemeProvider, Typography, createTheme } from "@mui/material";
 
 import { useParams } from "../Context/context";
+
+import * as d3 from "d3";
 
 const theme = createTheme({
   breakpoints: {
@@ -37,6 +39,12 @@ const DisplayInfo = () => {
     return convertedLanguage;
   };
 
+  const handleClick = () => {
+    const displayInfo = d3.select(".info-displayer");
+
+    displayInfo.style("transform", "translate(500px)");
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <Paper
@@ -44,7 +52,16 @@ const DisplayInfo = () => {
         style={{ transform: "translateX(500px)", transition: "transform 250ms ease-in-out" }}
         sx={{ width: { xs: window.innerWidth, sm: "400px" }, zIndex: 1337 }}>
         <aside>
+          <div style={{ display: "flex", justifyContent: "end" }}>
+            <IconButton
+              onClick={handleClick}
+              sx={{ width: "38px", height: "38px", fontWeight: 200, color: "black" }}>
+              X
+            </IconButton>
+          </div>
+
           <h2 style={{ textAlign: "center" }}>About Country</h2>
+
           <Card>
             <CardContent>
               <Typography variant="h6" component={"p"}>
