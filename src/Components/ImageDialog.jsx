c6b0a5c75fba94793c22899c8eea47b344a03540
PabloPicas0 <pawel.fe@gmail.com>
2023-07-02 22:51:02 +0200

    added main image to dialog component with credentials to them

--- src/Components/ImageDialog.jsx
+++ src/Components/ImageDialog.jsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardMedia, Dialog, DialogContent, DialogTitle, Slide, Typography } from "@mui/material";
+import { Card, CardContent, CardMedia, Dialog, Typography } from "@mui/material";
 import { useParams } from "../Utils/context";
 
 const ImageDialog = () => {
@@ -7,11 +7,36 @@ const ImageDialog = () => {
   const handleClose = () => {
     setOpen(false);
   };
+  console.log(images[imageIndex]);
+
+  if (images.length === 0) {
+    return;
+  }
 
   return (
-      <Dialog onClose={handleClose} open={open}>
-        {`test nr ${imageIndex}`}
-      </Dialog>
+    <Dialog onClose={handleClose} open={open} maxWidth={false}>
+      <Card sx={{ maxWidth: "960px" }}>
+        <CardMedia
+          component={"img"}
+          src={images[imageIndex].urls.regular}
+          alt={images[imageIndex].alt_description}
+        />
+        <CardContent style={{ padding: "10px", textAlign: "center " }}>
+          <Typography component={"p"}>
+            Photo by{" "}
+            <a
+              href={`${images[imageIndex].user.links.html}?utm_source=Around the world&utm_medium=referral`}
+              target="_blank">
+              {images[imageIndex].user.name}
+            </a>{" "}
+            on{" "}
+            <a href="https://unsplash.com/?utm_source=Around the world&utm_medium=referral" target="_blank">
+              Unsplash
+            </a>
+          </Typography>
+        </CardContent>
+      </Card>
+    </Dialog>
   );
 };
 
