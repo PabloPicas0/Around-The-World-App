a7e6eca3443ffb89957a4f1d95d9cebc2f6bbfb6
PabloPicas0 <pawel.fe@gmail.com>
2023-07-02 14:42:11 +0200

    abstracted makeInfoCall function, connected to unsplash api

--- src/Utils/fetchRequests.js
+++ src/Utils/fetchRequests.js
@@ -1,4 +1,4 @@
-export const makeInfoCall = async (id) => {
+const fetchBasicInfo = async (id) => {
   // Kosovo has no ccn3 id
   // switch to cca3 id
   if (!id) id = "KOS";
@@ -6,15 +6,40 @@ export const makeInfoCall = async (id) => {
   const basicInfoCall = await fetch(`https://restcountries.com/v3.1/alpha/${id}`);
   const basicInfoData = await basicInfoCall.json();
 
+  return basicInfoData;
+};
+
+const fetchWiki = async (name) => {
+  const wikiCall = await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${name}`);
+  const wikiData = await wikiCall.json();
+
+  const { extract } = wikiData;
+
+  return extract;
+};
+
+const fetchImages = async (name) => {
+  const imagesRequest = await fetch(`https://api.unsplash.com/search/photos/?query=${name}&per_page=8`, {
+    headers: {
+      Authorization: `Client-ID ${import.meta.env.VITE_ACCESS_KEY}`,
+    },
+  });
+
+  const images = await imagesRequest.json();
+
+  return images;
+};
+
+export const makeInfoCall = async (id) => {
+  const basicInfoData = await fetchBasicInfo(id);
+
   const { name } = basicInfoData[0];
 
   if (name.common === "DR Congo") name.common = "Democratic Republic of the Congo";
   if (name.common === "Czechia") name.common = "Czech Republic";
 
-  const wikiCall = await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${name.common}`);
-  const wikiData = await wikiCall.json();
-
-  const { extract } = wikiData;
+  const extract = await fetchWiki(name.common);
+  const images = await fetchImages(name.common);
 
   return [...basicInfoData, { extract: extract }];
 };
