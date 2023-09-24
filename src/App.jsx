251fa11d398d45bbf0362922d6a9b976bc3f9527
PabloPicas0 <pawel.fe@gmail.com>
2023-09-24 08:24:55 +0200

    perf: remove theme component

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,11 @@ import DisplayInfo from "./Components/DisplayInfo";
 
 import "./App.css";
 
-import { Box, Container, ScopedCssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { Box, Container } from "@mui/material";
 
 import { useParams } from "./Utils/context";
 
-import { useMemo } from "react";
 
-import getDesignTokens from "./Utils/designMode";
 import ImageDialog from "./Components/ImageDialog";
 
 const boxStyles = {
@@ -22,44 +20,22 @@ const boxStyles = {
 };
 
 function App() {
-  const { mode, showInfo } = useParams();
+  const { showInfo } = useParams();
   const { width } = window.screen;
 
-  const theme = useMemo(() => {
-    return createTheme({
-      breakpoints: {
-        values: {
-          xs: 0,
-          sm: 400,
-          md: 960,
-          lg: 1100,
-          xl: 1536,
-        },
-      },
-      palette: {
-        mode: mode,
-        ...getDesignTokens(mode),
-      },
-    });
-  }, [mode]);
-
   return (
-    <ThemeProvider theme={theme}>
-      <ScopedCssBaseline enableColorScheme>
-        <Container disableGutters maxWidth={false}>
-          <Box
-            sx={{
-              ...boxStyles,
-              overflow: (width < 960 && !showInfo) || width > 960 ? "hidden" : "visible",
-            }}>
-            <Navbar />
-            <RenderWorld />
-            <DisplayInfo />
-            <ImageDialog />
-          </Box>
-        </Container>
-      </ScopedCssBaseline>
-    </ThemeProvider>
+    <Container disableGutters maxWidth={false}>
+      <Box
+        sx={{
+          ...boxStyles,
+          overflow: (width < 960 && !showInfo) || width > 960 ? "hidden" : "visible",
+        }}>
+        <Navbar />
+        <RenderWorld />
+        <DisplayInfo />
+        <ImageDialog />
+      </Box>
+    </Container>
   );
 }
 
