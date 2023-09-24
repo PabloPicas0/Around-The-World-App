a087616f6e826eadb6077e88577b67d3a84be917
PabloPicas0 <pawel.fe@gmail.com>
2023-09-24 04:15:57 +0200

    refactor: delete unused style

--- src/styles/DisplayInfoStyles.js
+++ src/styles/DisplayInfoStyles.js
@@ -1,9 +1,7 @@
 export const conatinerStyles = {
-  position: "relative",
   overflowY: { xs: "visible", md: "auto" },
   transition: "transform 250ms ease-in-out",
   width: { xs: "auto", md: "500px" },
-  transform: { xs: "none", md: "translateX(500px)" },
 };
 
 export const buttonWrapperStyles = {
